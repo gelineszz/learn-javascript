b12078d118ec951376f8c778be5e49c3cedd5969
angeline-eldisc <angelineeldisc1317@gmail.com>
2023-11-23 14:15:57 +0700

    Basic Syntax

---
+++ chapter_1/script.js
@@ -0,0 +1,27 @@
+// Javascript known ad loosely type language and zero-based language
+// Strongly type language => Ruby and Python
+
+// Always end the line of the code with semi-colon to avoid errors  
+
+// Displays an alert box with a message and an OK button
+alert("hello world");
+
+// Write message to the browser console
+console.log(1);
+
+
+
+// Comments
+// Single line comment (Ctrl + /)
+
+/*
+ * Multi line comment
+ * Enter your line here
+ */
+
+
+
+
+// There is a technical difference between functions and methods
+// Function is a snippet of code which performs some kind of specific task
+// Method is a function that's associated with particular object or datatypes
---
+++ chapter_1/variables.js
@@ -0,0 +1,22 @@
+console.log("================== VARIABLES ==================");
+
+// Variables
+// let variable => newer version of "var"
+// let and var value can be changed
+let age = 20;
+let year = 2023;
+
+// Log things to console
+console.log(age, year);
+
+// Update the age value to 30
+age = 30;
+console.log(age);
+
+// The const value can not be changed
+const point = 100;
+console.log(point);
+
+// var is old data type (used in code written for older browsers)
+var score = 75;
+console.log(score);
